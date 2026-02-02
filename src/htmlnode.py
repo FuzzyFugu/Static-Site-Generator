128c98bf4d1a9e38bbb531443a1db8cee1c9739c
FuzzyFugu <andrei9290@gmail.com>
2026-02-02 21:35:03 +0000

    Day 2 commit

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -85,6 +85,31 @@ class ParentNode(HTMLNode):
             )
 
         raise Exception(f"Unsupported TextType: {text_node.text_type}")
+        
+    def split_nodes_delimiter(old_nodes, delimiter, text_type):
+        new_nodes = []
+
+        for node in old_nodes:
+            if node.text_type != TextType.TEXT:
+                new_nodes.append(node)
+                continue
+
+            parts = node.text.split(delimiter)
+
+            if len(parts) % 2 == 0:
+                raise ValueError(f"Invalid markdown: unclosed delimiter {delimiter}")
+
+            for i, part in enumerate(parts):
+                if part == "":
+                    continue
+
+                if i % 2 == 0:
+                    new_nodes.append(TextNode(part, TextType.TEXT))
+                else:
+                    new_nodes.append(TextNode(part, text_type))
+
+        return new_nodes
+
 
 
 
